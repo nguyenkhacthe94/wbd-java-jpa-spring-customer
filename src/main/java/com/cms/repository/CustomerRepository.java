7bf2ed742c890f28165a3683a1a1e8880811ffe0
Himedere <nguyenkhacthe94@gmail.com>
2018-05-30 10:15:54 +0700

    config config config

---
+++ src/main/java/com/cms/repository/CustomerRepository.java
@@ -0,0 +1,5 @@
+package com.cms.repository;
+import com.cms.model.Customer;
+
+public interface CustomerRepository extends Repository<Customer> {
+}
---
+++ src/main/java/com/cms/service/CustomerServiceImpl.java
@@ -0,0 +1,32 @@
+package com.cms.service;
+
+import com.cms.model.Customer;
+import com.cms.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class CustomerServiceImpl implements CustomerService {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Customer findById(Long id) {
+        return customerRepository.findById(id);
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    @Override
+    public void remove(Long id) {
+        customerRepository.remove(id);
+    }
+}
